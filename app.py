bec75c4cd862fe2c16a3eb40da96cf6e95d315f2
Blackwidow-sudo <bob_merlin92@web.de>
2024-03-26 19:41:04 +0100

    feat: impl zipper

--- app.py
+++ app.py
@@ -1,23 +1,65 @@
 from pathlib import Path
 import gradio as gr
+from zipfile import ZipFile
+import os
+import shutil
 
-def upload_file(filepath):
-    name = Path(filepath).name
-    return [gr.UploadButton(visible=False), gr.DownloadButton(label=f"Download {name}", value=filepath, visible=True)]
+INPUT_DIR = os.path.join(os.path.dirname(__file__), "IN")
+OUTPUT_DIR = os.path.join(os.path.dirname(__file__), "OUT")
+
+
+def clean_dir(directory):
+    for file in os.listdir(directory):
+        os.remove(os.path.join(directory, file))
+
+
+def make_zip(files):
+    zip_path = os.path.join(OUTPUT_DIR, "extracted.zip")
+
+    with ZipFile(zip_path, "w") as zip:
+        for file in files:
+            zip.write(os.path.join(INPUT_DIR, file), file)
+
+    return zip_path
+
+
+def upload_file(files):
+    clean_dir(INPUT_DIR)
+    clean_dir(OUTPUT_DIR)
+
+    for file in files:
+        shutil.move(file, INPUT_DIR)
+
+    zip = make_zip(os.listdir(INPUT_DIR))
+
+    return [
+        gr.Files(visible=False),
+        gr.DownloadButton(label="Download", value=zip, visible=True)
+    ]
+
 
 def download_file():
-    return [gr.UploadButton(visible=True), gr.DownloadButton(visible=False)]
+    return [
+        gr.Files(value=None, visible=True),
+        gr.DownloadButton(visible=False)
+    ]
+
 
 with gr.Blocks() as demo:
-    gr.Markdown("First upload a file and and then you'll be able download it (but only once!)")
+    gr.Markdown("Upload a file to extract the manuscripts.")
     with gr.Row():
-        u = gr.UploadButton("Upload a file", file_count="single")
-        d = gr.DownloadButton("Download the file", visible=False)
+        up = gr.Files(label="Upload manuscripts")
+        down = gr.DownloadButton("Download the file", visible=False)
 
-    u.upload(upload_file, u, [u, d])
-    d.click(download_file, None, [u, d])
+    up.upload(upload_file, up, [up, down], show_progress=True)
+    down.click(download_file, None, [up, down])
 
 
 if __name__ == "__main__":
-    demo.launch()
+    if not os.path.exists(INPUT_DIR):
+        os.mkdir(INPUT_DIR)
+    if not os.path.exists(OUTPUT_DIR):
+        os.mkdir(OUTPUT_DIR)
+    
+    demo.queue().launch()
 	
